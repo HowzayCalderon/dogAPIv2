ec87e2ebe603eee4bcfd144905c8c2b077cb0be5
hairyElephant <user@email.com>
2023-01-14 12:56:05 -0500

    Fixed Fetch issue

--- src/App.js
+++ src/App.js
@@ -14,22 +14,26 @@ function App() {
     fetch(`https://api-project-dogpics.up.railway.app/pug`)
     .then((res) => res.json())
     .then((data)=> setPugData(data))
+    .catch(err => {console.log(err.message)})
   }
 
   function apiCallShiba(){
     fetch(`https://api-project-dogpics.up.railway.app/shiba`)
     .then((res) => res.json())
     .then((data)=> setShibaData(data))
+    .catch(err => {console.log(err.message)})
   }
   function apiCallHusky(){
     fetch(`https://api-project-dogpics.up.railway.app/husky`)
     .then((res) => res.json())
     .then((data)=> setHuskyData(data))
+    .catch(err => {console.log(err.message)})
   }
   function apiCallCorgi(){
     fetch(`https://api-project-dogpics.up.railway.app/corgi`)
     .then((res) => res.json())
     .then((data)=> setCorgiData(data))
+    .catch(err => {console.log(err.message)})
   }
 
 
@@ -50,10 +54,10 @@ function App() {
 
   return (
     <div className="App">
-      <img src={pugs[0].message[0]} alt="Dog"></img>
-      <img src={shibas[0].message[0]} alt="Dog"></img>
-      <img src={huskies[0].message[0]} alt="Dog"></img>
-      <img src={corgis[0].message[0]} alt="Dog"></img>
+      <img src={pugs.length? pugs[0].message[0]: null} alt="Dog"></img>
+      <img src={shibas.length? shibas[0].message[0]: null} alt="Dog"></img>
+      <img src={huskies.length? huskies[0].message[0]: null} alt="Dog"></img>
+      <img src={corgis.length? corgis[0].message[0]: null} alt="Dog"></img>  
     </div>
   );
 }
