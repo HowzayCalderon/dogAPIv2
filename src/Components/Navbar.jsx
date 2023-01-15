df3a06c1e080d6b6d89e1cc02fe75cc7739d33f2
hairyElephant <user@email.com>
2023-01-14 20:56:46 -0500

    Each dog has their own page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,42 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import * as FaIcons from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 
 function Navbar() {
+    const [hidden, setDisplay] = useState(false)
+
+    const hideMenu = () => {
+        hidden? setDisplay(false): setDisplay(true)
+    }
+
   return (
     <>
       <div className="top">
-        <Link to="/" className="">
-          <FaIcons.FaBars />
+        <Link  onClick={hideMenu} className="">
+          {hidden? <FaIcons.FaBars />: <FaIcons.FaTimes />}
         </Link>
       </div>
-      <div className="list">
+      <div className={hidden? 'hidden': 'list'}>
         <nav>
           <ul>
             <li>
-              <Link to="/home">
-                <FaIcons.FaHome /> Home{" "}
+              <Link to="/">
+                <FaIcons.FaHome /> Home
               </Link>
             </li>
             <li>
-              <Link to="/corgi">
-                <FaIcons.FaDog /> Corgi{" "}
+              <Link to="/corgis">
+                <FaIcons.FaDog /> Corgis
               </Link>
             </li>
             <li>
-              <Link to="/husky">
-                <FaIcons.FaDog /> Husky
+              <Link to="/huskies">
+                <FaIcons.FaDog /> Huskies
               </Link>
             </li>
             <li>
-              <Link to="/shiba">
-                <FaIcons.FaDog /> Shiba
+              <Link to="/shibas">
+                <FaIcons.FaDog /> Shibas
               </Link>
             </li>
             <li>
-              <Link to="/pug">
-                <FaIcons.FaDog /> Pug
+              <Link to="/pugs">
+                <FaIcons.FaDog /> Pugs
               </Link>
             </li>
           </ul>
