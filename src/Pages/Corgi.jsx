05c7702a71e23c8b53484a009be99e000b9349f3
hairyElephant <josecalderon353@gmail.com>
2023-03-08 19:52:31 -0500

    not working yet

--- src/Pages/Corgi.jsx
+++ src/Pages/Corgi.jsx
@@ -2,49 +2,29 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import { useEffect, useState } from "react";
 import Modal from "../Components/Modal";
-import * as FaIcons from "react-icons/fa";
+import axios from "axios";
 
 function Corgi(props) {
   const [corgis, setCorgiData] = useState([]);
-  const [dogPic, setDogPic] = useState("");
-  const [displayMod, setDisplay] = useState("modal-container");
-  const [page, setPage] = useState(0);
+  const [picIndex, setIndex] = useState(0)
 
-  function apiCallCorgi() {
-    fetch(`https://dog.ceo/api/breed/corgi/images`)
-      .then((res) => res.json())
-      .then((data) => setCorgiData(data))
-      .catch((err) => {
-        console.log(err.message);
-      });
+  async function apiCallCorgi() {
+    try {
+      const response = await axios.get(
+        `https://dog.ceo/api/breed/corgi/images`
+      );
+      setCorgiData(response);
+      console.log(corgis.data.message);
+
+    } 
+    catch (error) {
+      console.error(error);
+    }
   }
+
   useEffect(() => {
     apiCallCorgi();
   }, []);
-
-  const handlePicClick = () => {
-    if (displayMod === "modal-container") {
-      setDisplay("modal-container-active");
-    } else {
-      setDisplay("modal-container");
-    }
-  };
-
-  const handleNextClick = () => {
-    if (page + 24 < corgis.message.length) {
-      setPage((prev) => prev + 24);
-    } else {
-      setPage(0);
-    }
-  };
-
-  const handlePrevClick = () => {
-    if (page - 24 < corgis.message.length) {
-      setPage((prev) => prev - 16);
-    } else {
-      setPage(0);
-    }
-  };
 
   return (
     <div className="main-page-con">
@@ -52,45 +32,10 @@ function Corgi(props) {
       <div className="title-container">
         <h1>Corgis</h1>
       </div>
-      <Modal
-        display={displayMod}
-        handleclick={() => handlePicClick()}
-        dogPic={dogPic}
-      />
-      <div className="pic-container">
-        {corgis.message.length
-          ? corgis.message.slice(page, page + 24).map((pic, index) => {
-              return (
-                <div>
-                  <img
-                    key={index}
-                    src={pic}
-                    onClick={() => {
-                      setDogPic(pic);
-                      if (displayMod === "modal-container") {
-                        setDisplay("modal-container-active");
-                      } else {
-                        setDisplay("modal-container");
-                      }
-                    }}
-                    alt="dog"
-                  ></img>
-                </div>
-              );
-            })
-          : null}
-        <FaIcons.FaArrowCircleLeft
-          className="Arrow"
-          onClick={() => {
-            handlePrevClick();
-          }}
-        />
-        <FaIcons.FaArrowCircleRight
-          onClick={() => {
-            handleNextClick();
-          }}
-          className="Arrow"
-        />
+      <div className="outerContainer">
+        <div className="innerContainer">
+          <img src={corgis.data.message[picIndex]} alt="" />
+        </div>
       </div>
     </div>
   );
